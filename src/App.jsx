1628e51bbd46f60eb335aaee42997d6f3e9df106
Samuel <gonzalez.samuelalejandro@outlook.com>
2025-04-05 15:30:01 -0300

    We finish the Skills section and redesign teh buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './assets/styles/global.css'
 
 //Components
 import { Hero } from './features/Hero/Hero'
-
+import { Skills } from './features/Skills/Skills'
 function App() {
 
   return (
     <>
       <Hero />
+      <Skills />
     </>
   )
 }
---
+++ src/features/Skills/Skills.jsx
@@ -0,0 +1,72 @@
+import "./Skills.css";
+
+//Icons
+import html from "../../assets/images/html.png";
+import css from "../../assets/images/css.png";
+import javascript from "../../assets/images/javascript.png";
+import react from "../../assets/images/react.png";
+import nodejs from "../../assets/images/nodejs.png";
+import express from "../../assets/images/express-big.png";
+import mongo from "../../assets/images/mongo.png";
+import git from "../../assets/images/git.png";
+import powershell from "../../assets/images/powershell.png";
+import vsc from "../../assets/images/vsc-big.png";
+
+function Skills() {
+  return (
+    <section class="skills-container">
+      <div class="skills-content">
+        <div class="skills-header">
+          <h2 class="skills-title">Habilidades</h2>
+          <p class="skills-description">
+            Una selección de tecnologías y herramientas que domino y aplico en
+            proyectos reales.
+          </p>
+        </div>
+        <div className="skills-box">
+          <div className="skill">
+            <img src={html} alt="" className="skill-img" />
+          </div>
+
+          <div className="skill">
+            <img src={css} alt="" className="skill-img" />
+          </div>
+
+          <div className="skill">
+            <img src={javascript} alt="" className="skill-img" />
+          </div>
+
+          <div className="skill">
+            <img src={react} alt="" className="skill-img" />
+          </div>
+
+          <div className="skill">
+            <img src={nodejs} alt="" className="skill-img" />
+          </div>
+
+          <div className="skill">
+            <img src={express} alt="" className="skill-img" />
+          </div>
+
+          <div className="skill">
+            <img src={mongo} alt="" className="skill-img" />
+          </div>
+
+          <div className="skill">
+            <img src={git} alt="" className="skill-img" />
+          </div>
+
+          <div className="skill">
+            <img src={powershell} alt="" className="skill-img" />
+          </div>
+
+          <div className="skill">
+            <img src={vsc} alt="" className="skill-img" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export { Skills };
